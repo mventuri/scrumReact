3f469721fb7cf16a149468f70a87cf3845b792ae
Marco Venturi <mventuri@hotmail.it>
2019-03-27 15:13:15 +0100

    Bootstrap Cards

--- src/Components/SprintReview.js
+++ src/Components/SprintReview.js
@@ -3,15 +3,22 @@ import React, { Component } from "react";
 class SprintReview extends Component {
   render() {
     return (
-      <div className="col-xs-12 col-md-3">
-        <h1> Sprint Planning </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.Integer risus
-          felis, volutpat sit amet dictum in , dapibus nec enim.Sed quis eros et
-          magna tempor scelerisque.Vestibulum quis urna a libero dapibus
-          tristique.Vestibulum pellentesque, metus rhoncus ornare ornare, massa
-          sapien placerat urna, vitae tincidunt urna ipsum in justo.{" "}
-        </p>
+      <div className="col-xs-12 col-md-3 card">
+        <div class="card-body">
+          <img
+            class="card-img-top"
+            src="https://images.unsplash.com/photo-1553640849-9cc4fea9735b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            alt="Card image cap"
+          />
+          <h5 class="card-title">Sprint Review</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            More
+          </a>
+        </div>
       </div>
     );
   }
